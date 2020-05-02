b9b927d0bb08df31a4807985f7b2164c5e138b80
Luuk Verweij <luuk_verweij@msn.com>
2020-05-02 23:42:42 +0200

    optimise registers by using uint instead of uint16

--- pkg/gb/cpu.go
+++ pkg/gb/cpu.go
@@ -1,19 +1,15 @@
 package gb
 
-import (
-	"github.com/Humpheh/goboy/pkg/bits"
-)
-
 // Register represents a GB CPU 16bit register which provides functions
 // for setting and getting the higher and lower bytes.
 type register struct {
 	// The value of the register.
-	value uint16
+	value uint
 
 	// A mask over the possible values in the register.
 	// Only used for the AF register where lower bits of
 	// F cannot be set.
-	mask uint16
+	mask uint
 }
 
 // Hi gets the higher byte of the register.
@@ -28,24 +24,24 @@ func (reg *register) Lo() byte {
 
 // HiLo gets the 2 byte value of the register.
 func (reg *register) HiLo() uint16 {
-	return reg.value
+	return uint16(reg.value)
 }
 
 // SetHi sets the higher byte of the register.
 func (reg *register) SetHi(val byte) {
-	reg.value = uint16(val)<<8 | (uint16(reg.value) & 0xFF)
+	reg.value = uint(val)<<8 | (reg.value & 0xFF)
 	reg.updateMask()
 }
 
 // SetLog sets the lower byte of the register.
 func (reg *register) SetLo(val byte) {
-	reg.value = uint16(val) | (uint16(reg.value) & 0xFF00)
+	reg.value = uint(val) | (reg.value & 0xFF00)
 	reg.updateMask()
 }
 
 // Set the value of the register.
 func (reg *register) Set(val uint16) {
-	reg.value = val
+	reg.value = uint(val)
 	reg.updateMask()
 }
 
@@ -87,12 +83,19 @@ func (cpu *CPU) Init(cgb bool) {
 }
 
 // Internally set the value of a flag on the flag register.
-func (cpu *CPU) setFlag(index byte, on bool) {
+func (cpu *CPU) setFlag(index int, on bool) {
+	mask := uint(1 << index)
 	if on {
-		cpu.AF.SetLo(bits.Set(cpu.AF.Lo(), index))
+		cpu.AF.value |= mask
 	} else {
-		cpu.AF.SetLo(bits.Reset(cpu.AF.Lo(), index))
+		cpu.AF.value &^= mask
 	}
+	cpu.AF.updateMask()
+}
+
+func (cpu *CPU) getFlag(index int) bool {
+	mask := uint(1 << index)
+	return cpu.AF.value&mask != 0
 }
 
 // SetZ sets the value of the Z flag.
@@ -117,20 +120,20 @@ func (cpu *CPU) SetC(on bool) {
 
 // Z gets the value of the Z flag.
 func (cpu *CPU) Z() bool {
-	return cpu.AF.HiLo()>>7&1 == 1
+	return cpu.getFlag(7)
 }
 
 // N gets the value of the N flag.
 func (cpu *CPU) N() bool {
-	return cpu.AF.HiLo()>>6&1 == 1
+	return cpu.getFlag(6)
 }
 
 // H gets the value of the H flag.
 func (cpu *CPU) H() bool {
-	return cpu.AF.HiLo()>>5&1 == 1
+	return cpu.getFlag(5)
 }
 
 // C gets the value of the C flag.
 func (cpu *CPU) C() bool {
-	return cpu.AF.HiLo()>>4&1 == 1
+	return cpu.getFlag(4)
 }
